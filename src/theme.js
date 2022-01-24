6cdd5b6477abc3e13cead4d39b1d71cec41d20d6
MYKYTA RYSIEV <nickpolska21@gmail.com>
2022-01-24 21:14:58 +0100

    DE version of project added

---
+++ src/theme.js
@@ -0,0 +1,16 @@
+export const theme = {
+  color: {
+    white: "#fff",
+    black: "#000",
+    deepSkyBlue: "#00bfff",
+    crimson: "#ec365b",
+    lightSlateGrey: "#778899",
+    lightGrey: "#d3d3d3",
+    // yellow: "#ffff00",
+    // forestGreen: "#228b22",
+  },
+  breakpoint: {
+    mobileMax360: 360,
+    mobileMax414: 414,
+  },
+};
